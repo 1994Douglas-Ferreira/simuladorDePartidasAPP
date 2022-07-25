c881d88e125ab0df46c35d9a3f79acfcd27eebc6
Ferreira <douglas.ferreira@hotmail.com>
2022-07-25 08:55:06 -0300

    Finalizando o projeto!!

---
+++ app/src/main/java/com/DIO/simulator/ui/adapter/MatchesAdapter.java
@@ -0,0 +1,80 @@
+package com.DIO.simulator.ui.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.DIO.simulator.databinding.MatchItemBinding;
+import com.DIO.simulator.domain.Match;
+import com.DIO.simulator.ui.DetailActivity;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class MatchesAdapter extends RecyclerView.Adapter<MatchesAdapter.ViewHolder> {
+
+    private List<Match> matches;
+
+    public MatchesAdapter(List<Match> matches) {
+        this.matches = matches;
+    }
+
+    public List<Match> getMatches() {
+        return matches;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        MatchItemBinding binding = MatchItemBinding.inflate(layoutInflater,parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Context context = holder.itemView.getContext();
+
+        Match match = matches.get(position);
+
+        //Adapta os dados da partida (Recuperando do API) para nosso layout
+        Glide.with(context).load(match.getHomeTeam().getImage()).circleCrop().into(holder.binding.ivHomeTeam);
+        holder.binding.tvHomeName.setText(match.getHomeTeam().getName());
+
+        if(match.getHomeTeam().getScore() != null) {
+            holder.binding.tvHomeScore.setText(String.valueOf(match.getHomeTeam().getScore()));
+        }
+
+        Glide.with(context).load(match.getAwayTeam().getImage()).circleCrop().into(holder.binding.ivAwayTeam);
+        holder.binding.tvAwayName.setText(match.getAwayTeam().getName());
+
+        if(match.getAwayTeam().getScore() != null) {
+            holder.binding.tvAwayScore.setText(String.valueOf(match.getAwayTeam().getScore()));
+        }
+
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.putExtra(DetailActivity.Extras.MATCH, match);
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return matches.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final MatchItemBinding binding;
+
+        public ViewHolder(MatchItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
